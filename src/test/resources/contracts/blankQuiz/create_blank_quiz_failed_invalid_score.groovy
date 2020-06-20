6f00005948541e10b88f4372d6fb7f2bc4bed6c4
Yuan Shenjian <sjyuan@thoughtworks.com>
2020-06-20 17:54:12 +0800

    Task2.1: Create blank quiz failed contract to be used as stub service
    Signed-off-by: Yuan Shenjian <sjyuan@thoughtworks.com>

---
+++ src/test/resources/contracts/blankQuiz/create_blank_quiz_failed_invalid_score.groovy
@@ -0,0 +1,40 @@
+package blankQuiz
+
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    description '''
+Represents creating a new blankQuiz
+
+given:
+    an invalid score
+when:
+    a teacher create a new paper
+then:
+    this teacher should get bad request
+'''
+
+    request {
+        url "/quizzes"
+        method POST()
+        headers {
+            contentType applicationJson()
+        }
+        body(
+                teacherId: $(consumer(regex('[a-zA-Z0-9]{36}'))),
+                question: $(consumer(regex('.{1,255}'))),
+                score: 110,
+                referenceAnswer: $(consumer(regex('.{1,4000}')))
+        )
+    }
+
+    response {
+        status BAD_REQUEST()
+        headers {
+            contentType applicationJson()
+        }
+        body(
+                errorMsg: 'Invalid Score',
+        )
+    }
+}
